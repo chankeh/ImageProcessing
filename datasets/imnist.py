50d45c1843d50d23a0de98f9b3324c1b66c2e566
Junhui Zhang <mrlittlezhu@gmail.com>
2017-04-30 13:52:25 +0800

    优化目录结构，上传mnist图片数据

--- datasets/imnist.py
+++ datasets/imnist.py
@@ -1,20 +1,3 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Functions for downloading and reading MNIST data."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -27,11 +10,9 @@ from tensorflow.python.framework import random_seed
 from PIL import Image
 from utils.labelFile2Map import *
 
-
 def _read32(bytestream):
   dt = numpy.dtype(numpy.uint32).newbyteorder('>')
   return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
-
 
 def process_images(label_file, one_hot=False, num_classes=10):
     if file.getFileName(label_file) == 'train.txt':
